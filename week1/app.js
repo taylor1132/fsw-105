13627ef1ad1cab755dbb69492a0bd49f308b239e
taylor1132 <taylor.higginson@bryanuniversity.edu>
2020-09-05 10:39:36 -0600

    first commit

---
+++ week1/app.js
@@ -0,0 +1,32 @@
+var shopper = {
+
+    var store = "walmart"
+
+    var storeAge = 5
+
+    var isOpen = true
+
+    var isClosed = false 
+
+
+    var person = {
+
+        firstName: "Taylor",
+        lastName: "Higginson",
+        id: 3334,
+        fullname: function() {
+            return this.firstName + " " + this.lastName;
+        }
+        
+    };
+
+
+}
+    var groceryCart = {
+
+        name: "grapes",
+        name: "hamberger",
+        name: "chicken",
+        name:"watermelon",
+        name:"milk"
+    }
