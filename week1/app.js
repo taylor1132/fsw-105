158fb87dfe6560ed5f88dee7abda259acb6eaae6
taylor1132 <taylor.higginson@bryanuniversity.edu>
2020-09-06 10:25:38 -0600

    second commit

--- week1/app.js
+++ week1/app.js
@@ -1,32 +1,29 @@
-var shopper = {
-
-    var store = "walmart"
-
-    var storeAge = 5
-
-    var isOpen = true
-
-    var isClosed = false 
+ 
 
 
-    var person = {
+    var shopper = {
 
         firstName: "Taylor",
         lastName: "Higginson",
         id: 3334,
-        fullname: function() {
+        isAlive: true,
+        groceryCart:[apples,bananas,milk,beef,chips,water],
+        console.log(shopper.fullname()); 
             return this.firstName + " " + this.lastName;
         }
-        
-    };
 
+// console.log ()
+// primimitive 
+        //strings  ex. var name = "joe"  needs to be in quotes 
+        //numbers  ex. var age = 20
+        //booleans = true or false (var will usually ask a question) var isAlive = true, var hasDoneTaxes = false
 
-}
-    var groceryCart = {
-
-        name: "grapes",
-        name: "hamberger",
-        name: "chicken",
-        name:"watermelon",
-        name:"milk"
-    }
+// complex 
+        //array - list of similar items  ex var numbers [1,2,3,4,5,6,7] (0 indexed)
+        // objects - explains a similar item in depth  
+        // ex var person {
+        // name: "steve",
+        // age: 21, 
+        // hairColor: "brown",
+        // isAlive: true
+        //   }
